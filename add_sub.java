7d37d14e7493705f45ad9638076708f0d76e7da5
chithra0831 <chithra0831@gmmail.com>
2025-03-25 22:33:23 +0530

    this is my file

---
+++ add_sub.java
@@ -0,0 +1,13 @@
+public class add_sub {
+    public static void main(String[] args) {
+        int a=10;
+        double b=20;
+    
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+    }
+    
