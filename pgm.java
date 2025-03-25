d276ffcaf6cb90a6be2285cc2b29ff86f879b706
chithra0831 <chithrasm0831@gmail.com>
2025-03-25 23:07:44 +0530

    this is my code

---
+++ pgm.java
@@ -0,0 +1,15 @@
+public class pgm{
+    public static void main(String[] args) {
+        int a=10;
+        double b=20;
+    
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+    }
+    
+    
+
